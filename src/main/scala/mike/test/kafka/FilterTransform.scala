26c8357f13c8d2fb05ed71f32533e036d1a815e8
Mike Pickering <mpickering@echo360.com>
2019-08-08 07:55:02 -0400

    Clean up examples

--- src/main/scala/mike/test/kafka/FilterTransform.scala
+++ src/main/scala/mike/test/kafka/FilterTransform.scala
@@ -9,32 +9,27 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import java.time.Duration
 import java.util.Properties
 
-object FilterTransform {
+object FilterTransform extends App {
+    val bootstrap = if (args.length > 0) args(0) else "localhost:9092"
 
-    def main(args: Array[String]) {
-        val bootstrap = if (args.length > 0) args(0) else "localhost:9092"
+    // Stream setup; note that in Scala, we do not need to configure DEFAULT_KEY_SERDE_CLASS_CONFIG or
+    // DEFAULT_VALUE_SERDE_CLASS_CONFIG as in Java; the implicits in Serdes handle this
+    val props = new Properties
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap)
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-transformer-scala")
 
-        // Stream setup
-        val props = new Properties
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap)
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-transformer-scala")
+    val builder = new StreamsBuilder
+    val input = builder.stream[String, String]("source-topic")
 
-        val builder = new StreamsBuilder
-        val input = builder.stream[String, String]("source-topic")
+    input.map((_, value) => (value, value.toLowerCase()))
+      .to("transformed-topic")
 
-        input.map((_, value) => (value, value.toLowerCase()))
-          .to("transformed-topic")
+    input.filter((_, value) => value.length > 0 && (value.charAt(0) != '#')).to("filtered-topic")
 
-        input.filter((_, value) => value.length > 0 && (value.charAt(0) != '#')).to("filtered-topic")
+    val streams = new KafkaStreams(builder.build, props)
+    streams.start()
 
-        val streams = new KafkaStreams(builder.build, props)
-        streams.start()
-
-        sys.ShutdownHookThread {
-            streams.close(Duration.ofSeconds(10))
-        }
+    sys.ShutdownHookThread {
+        streams.close(Duration.ofSeconds(10))
     }
-
 }
--- src/main/scala/mike/test/kafka/WordCountMain.scala
+++ src/main/scala/mike/test/kafka/WordCountMain.scala
@@ -9,32 +9,26 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import java.time.Duration
 import java.util.Properties
 
-object WordCountMain {
+object WordCountMain extends App {
+    val bootstrap = if (args.length > 0) args(0) else "localhost:9092"
 
-    def main(args: Array[String]) {
-        val bootstrap = if (args.length > 0) args(0) else "localhost:9092"
+    // Stream setup
+    val props = new Properties
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap)
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-transformer-scala-wordcount")
 
-        // Stream setup
-        val props = new Properties
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap)
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-transformer-scala-wordcount")
+    val builder = new StreamsBuilder
+    val textLines = builder.stream[String, String]("streams-plaintext-input")
+    val wordCounts = textLines
+      .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+      .groupBy((_, word) => word)
+      .count()
+    wordCounts.toStream.to("streams-wordcount-output")
 
-        val builder = new StreamsBuilder
-        val textLines = builder.stream[String, String]("streams-plaintext-input")
-        val wordCounts = textLines
-          .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
-          .groupBy((_, word) => word)
-          .count()
-        wordCounts.toStream.to("streams-wordcount-output")
+    val streams = new KafkaStreams(builder.build, props)
+    streams.start()
 
-        val streams = new KafkaStreams(builder.build, props)
-        streams.start()
-
-        sys.ShutdownHookThread {
-            streams.close(Duration.ofSeconds(10))
-        }
+    sys.ShutdownHookThread {
+        streams.close(Duration.ofSeconds(10))
     }
-
 }
